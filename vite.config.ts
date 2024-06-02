90703b5d2e9626cbac4040375a8d383f8881d644
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-06-02 22:22:18 +0200

    [feature] Add Ui

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Niko-Sajadi-Vue-users/', // Set base path relative to domain root
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
