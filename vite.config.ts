095d59732b66d7097e385d71eb190c2c3addb7d9
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-06-02 20:52:54 +0200

    [feature] add deploying on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/Niko-Sajadi-Vue-users/', // Set base path relative to domain root
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
