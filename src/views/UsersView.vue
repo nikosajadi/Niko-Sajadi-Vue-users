7cecbfc2eb981815d0dbccba8d51c6bba89545ee
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-06-02 18:25:11 +0200

    [feature] Add Axios packag & get page2

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,8 +1,75 @@
-<script setup>
+<script>
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await axios.get('https://reqres.in/api/users?page=2');
+        this.users = response.data.data;
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    }
+  }
+};
 </script>
+
+
 <template>
-<div>hello Users </div>
-</template>
-<style>
-</style>
+    <div>
+      <h1>Users List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.first_name }}</td>
+            <td>{{ user.last_name }}</td>
+            <td><img :src="user.avatar" alt="User Avatar" /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+ 
+  
+  <style>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  
+  th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+  
+  th {
+    background-color: #f2f2f2;
+  }
+  
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+  </style>
